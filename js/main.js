7fe7accd2f24c23172433baba76812ad09ba23c0
Shannon Hawn <shannonhawn@hotmail.com>
2016-04-20 15:30:54 -0400

    deleted code - did not work

--- js/main.js
+++ js/main.js
@@ -4,11 +4,3 @@ var $graph = $('.graph');
 var $gas = $('.gas');
 var $speech = $('.speech');
 var $arrow = $('.arrow');
-
-$('.arrow').on('mouseover', function() {
-  $('.arrowPointer').addClass('colourArrow');
-});
-
-$('.arrow').on('mouseout', function() {
-  $('.arrowPointer').removeClass('colourArrow');
-});
