4fe06c5f9e488df35db4b8635eebdc539d5cdc54
Shannon Hawn <shannonhawn@hotmail.com>
2016-04-20 22:35:45 -0400

    made large gas can tilt - move over not working

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 var $body = $('body');
+var $btnMove = $('.btn-move');
+var $diamond = $('.diamond');
+var $gas = $('.gascan');
 
 $('html').on('keydown', function (e) {
   var $foot;
@@ -14,10 +17,25 @@ $('html').on('keydown', function (e) {
   }
 });
 
-  $body.on('click', '.foot', function () {
-    $(this).addClass('is-walking');
-  });
+$body.on('click', '.foot', function () {
+  $(this).addClass('is-walking');
+});
 
-  $body.on('transition', '.foot', function () {
-    $(this).remove();
-  });
+$body.on('transition', '.foot', function () {
+  $(this).remove();
+});
+
+
+$btnMove.on('click', function () {
+  $diamond.toggleClass('btn-slide');
+});
+
+$gascan.on('mouse-over', function() {
+  $gascan.toggleClass('gas-tilt');
+  fadeOut($(this).parents('svg'));
+});
+
+$gascan.on('mouse-out', function (){
+  $gascan.removeClass('gas-tilt');
+  fadeIn();
+})
