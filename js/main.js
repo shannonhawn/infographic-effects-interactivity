c381b585861a4f13bbab1cbd7b7d4896b9859a9d
Shannon Hawn <shannonhawn@hotmail.com>
2016-04-20 20:39:01 -0400

    added transition - trying to get green foot to walk across screen

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
+var $body = $('body');
 
-var $roundcar = $('.roundcar');
-var $graph = $('.graph');
-var $gas = $('.gas');
-var $speech = $('.speech');
+$('html').on('keydown', function (e) {
+  var $foot;
+
+  if (e.keyCode == 32) {
+    $foot = $('<div>');
+    $foot.addClass('foot');
+    $foot.css({
+      'top': Math.random() * (document.documentElement.clientHeight - 100),
+      'left': Math.random() * (document.documentElement.clientHeight - 100)
+    });
+    $body.append($foot);
+  }
+});
+
+  $body.on('click', '.foot', function () {
+    $(this).addClass('is-walking');
+  });
+
+  $body.on('transition', '.foot', function () {
+    $(this).remove();
+  });
