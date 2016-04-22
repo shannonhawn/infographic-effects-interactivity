acd0c3f544cac86323d6ce454ab0b8be036e43d0
Shannon Hawn <shannonhawn@hotmail.com>
2016-04-22 00:15:33 -0400

    added animation to arrow to shoot, trying to change colour of gas spill

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 var $body = $('body');
-var $gas = $('.gascan');
-var $arrow = $('.arrow');
+var $gascan = $('.gascan');
 
-var $foot = $('.foot');
-var $footprint = $(.'footprint');
-var $foothover = $(.'foothover');
+var $arrowshoot = $('.arrow-shoot');
+
 
 var fadeOut;
 var fadeIn;
@@ -12,39 +10,20 @@ var fadeIn;
 
 /*--Footstep--*/
 
-$foothover.on('click', function() {
-  $foot.toggleClass('foot-grow');
-  fadeOut($(this));
+$arrowshoot.on('click', function() {
+  $arrowshoot.toggleClass('arrow-shoot');
 });
 
-$foothover.on('mouseout', function() {
-  fadeIn();
+$arrowshoot.on('mouseout', function() {
+  $arrowshoot.removeClass('arrow-shoot');
 })
-
-function fadeOut (exclude) {
-  $('svg').not(exclude).addClass('fade');
-};
-
-function fadeIn () {
-  $('svg').removeClass('fade');
-};
-
-
-
-
-
-/*--Arrow--*/
-
-$arrow.waypoint(function() {
-  $arrow.addClass('js-appear-animate');
-}, { offset: '75%'});
 
 
 /*--Gas Spill--*/
 
 $gascan.on('mouse-over', function() {
   $gascan.toggleClass('gas-tilt');
-  fadeOut($(this).parents('svg'));
+  fadeOut();
 });
 
 $gascan.on('mouse-out', function (){
